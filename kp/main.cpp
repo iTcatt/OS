bb0e11122a64d829bffe1dd522e999622550c4c9
Iurii Kataev <iurii.kataev@gmail.com>
2023-01-04 17:21:50 +0300

    Релизовал передачу числа из child в parent

--- kp/main.cpp
+++ kp/main.cpp
@@ -25,9 +25,7 @@ int main(int argc, char* argv[]) {
 
     vector<Configuration> configs;
     configs = IniParser(argv[1]);
-    // for (auto config: configs) {
-    //     config.printConfiguration();
-    // }
+
     Graph dag_jobs = CreateGraph(configs);
     if (dag_jobs.isEmpty()) {
         cout << "The graph is empty!\n";
@@ -51,19 +49,59 @@ int main(int argc, char* argv[]) {
     map<int, string> job_paths = CreateDictionary(configs);
     cout << '\n';
 
+    // for (auto id: path) {
+    //     cout << id << ' ';
+    // }
+    // cout << '\n';
+    int number = 1;
     for (int id = path.size() - 1; id >= 0; --id) {
-        pid_t processID = fork();
-        if (processID < 0) {
-            printf("Error: fork\n");
-            return 1;
+        int pipe_parent[2];
+        int pipe_child[2];
+        if (pipe(pipe_parent) == -1) {
+            fprintf(stderr, "Pipe Failed");
+            exit(1);
         }
 
-        if (processID > 0) {
-            wait(NULL); 
+        if (pipe(pipe_child) == -1) {
+            fprintf(stderr, "Pipe Failed");
+            exit(1);
+        }
+        cout << "Ha " << path[id] << '\n';
+        pid_t process_id = fork();
+        
+        if (process_id < 0) {
+            printf("Error: Fork\n");
+            exit(1);
+        }
+
+        if (process_id > 0) {
+            close(pipe_parent[0]);
+            write(pipe_parent[1], &number, sizeof(int));
+            close(pipe_parent[1]);
+            wait(NULL);
+            
+            close(pipe_child[1]);
+            read(pipe_child[0], &number, sizeof(int));
+            close(pipe_child[0]);
+            
         } else {
-            execl(job_paths[path[id]].c_str(), job_paths[path[id]].c_str(), NULL);
-        } 
+            close(pipe_parent[1]);
+
+            if (dup2(pipe_child[1], 2) == -1) {
+                printf("Error: Dup2\n");
+                exit(2);
+            }
+
+            int received;
+            read(pipe_parent[0], &received, sizeof(int));
+            close(pipe_parent[0]);
+            
+            close(pipe_child[0]);
+            char str[2];
+            sprintf(str, "%d", received);
+            execl(job_paths[path[id]].c_str(), job_paths[path[id]].c_str(), str, NULL);
+        }
+        cout << number << '\n';
     }
-    
     return 0;
 }
